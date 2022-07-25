dba3ad508e90b9fbfa4eecf133d6425ebd5b7f26
Harry Wixley <hwixley1@gmail.com>
2022-07-25 14:50:18 +0100

    skills done

--- index.php
+++ index.php
@@ -618,8 +618,40 @@
                 </div>
             </section>
         <!--</div>-->
-        <section id="skills" name="skills" class="card skills-pseudo">
+        <section id="skills" name="skills" class="card">
             <h2>Skills</h2>
+            <div class="tools">
+                <h6>Tools & Technologies</h6>
+                <div class="tool-icons">
+                    <img src="https://img.icons8.com/color/48/000000/python.png" alt="Python" width="26px"><img src="https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png" alt="Java" width="26px">
+                    <img src="https://img.icons8.com/fluent/48/000000/swift.png" alt="Swift" width="26px">
+                    <img src="https://img.icons8.com/color/64/000000/javascript.png" alt="Javascript" width="26px">
+                    <img src="https://img.icons8.com/fluent/48/000000/matlab.png" alt="Matlab" width="26px">
+                    <img src="https://img.icons8.com/color/48/000000/c-programming.png" alt="C" width="26px">
+                    <img src="https://img.icons8.com/color/48/000000/haskell.png" alt="Haskell" width="26px">
+                    <img src="https://bs-uploads.toptal.io/blackfish-uploads/components/skill_page/content/logo_file/logo/195568/sql-64a6e0f07773cf17581e76ca09e17dbc.png" alt="SQL" width="26px">
+                    <img src="https://www.pngrepo.com/png/312259/512/github.png" alt="Git" width="26px">
+                    <img src="https://img.icons8.com/plasticine/64/000000/console.png" alt="Terminal" width="26px">
+                    <img src="https://img.icons8.com/color/48/000000/linux.png" alt="Linux" width="26px">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/1/15/Robot_Operating_System_logo.svg" alt="ROS" width="20px">
+                    <img src="https://isabelle.in.tum.de/img/isabelle.png" alt="Isabelle" width="26px">
+                    <img src="https://www.codex.academy/img/tech-logos/firebase.png" alt="Firebase" width="26px">
+                    <img src="https://avatars.githubusercontent.com/u/874086?s=280&v=4" alt="Junit" width="26px">
+                    <img src="https://cdn.iconscout.com/icon/free/png-256/php-3629567-3032350.png" alt="PHP" width="30px">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png" alt="Vue.js" width="26px">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png" alt="Laravel" width="26px">
+                    <img src="https://res.cloudinary.com/postman/image/upload/t_team_logo/v1/team/2893aede23f01bfcbd2319326bc96a6ed0524eba759745ed6d73405a3a8b67a8" alt="Postman" width="26px">
+                    <img src="https://biopython.org/assets/images/biopython_logo_white.png" alt="Biopython" height="26px">
+                    <img src="https://pytorch.org/assets/images/pytorch-logo.png" alt="Pytorch" width="26px">
+                    <img src="https://cdn-images-1.medium.com/max/1200/1*iDQvKoz7gGHc6YXqvqWWZQ.png" alt="Tensorflow" width="26px">
+                    <img src="https://pluralsight2.imgix.net/paths/images/nodejs-45adbe594d.png" alt="NodeJS" width="26px">
+                    <img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_plain_wordmark_logo_icon_146423.png" alt="MongoDB" width="26px">
+                </div>
+            </div>
+            <div class="tools">
+                <h6>Languages</h6>
+                <img class="langs" src="https://github.com/hwixley/github-stats/raw/master/generated/languages.svg"></img>
+            </div>
         </section>
         <section id="contact" name="contact" class="card">
             <h2>Contact Me Via Email</h2>
